8edc81c88084a99dd5eff2ccf2bbae563d5d717b
Dean Todoroski <de_tod@ymail.com>
2023-12-23 21:08:48 +0100

    Test pg/redis conn

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,12 +1,43 @@
 package main
 
 import (
+	"context"
+	"fmt"
+	"log"
 	"net/http"
+	"os"
 
 	"github.com/gin-gonic/gin"
+	"github.com/jackc/pgx/v5/pgxpool"
+	"github.com/redis/go-redis/v9"
 )
 
+var ctx = context.Background()
+
 func main() {
+	urlExample := fmt.Sprintf("postgres://postgres:asd9fwepub83lf@postgres:5432/bestwallet")
+	conn, err := pgxpool.New(ctx, urlExample)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Unable to connect to database: %v\n", err)
+		os.Exit(1)
+	}
+	defer conn.Close()
+	log.Println("Successfuly connected to postgres")
+
+	rdb := redis.NewClient(&redis.Options{
+		Addr:     "redis:6379",
+		Password: "", // no password set
+		DB:       0,  // use default DB
+	})
+	defer func() {
+		if err := rdb.Close(); err != nil {
+			log.Println("failed to close redis client")
+		}
+		log.Println("Closed redis client")
+	}()
+	res, err := rdb.Ping(ctx).Result()
+	log.Println("Redis ping returned: ", res, err)
+
 	r := gin.Default()
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
@@ -14,5 +45,5 @@ func main() {
 		})
 	})
 
-	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")aa
+	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
