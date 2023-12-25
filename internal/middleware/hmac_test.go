b6fcb62ebf0fb9e3756228eea0dc3b2876c50435
Dean Todoroski <de_tod@ymail.com>
2023-12-25 18:15:16 +0100

    Add empty body test case

--- internal/middleware/hmac_test.go
+++ internal/middleware/hmac_test.go
@@ -160,12 +160,47 @@ func TestHMACVerifier_InvalidKeyID(t *testing.T) {
 	method, path := "GET", "/sut"
 	router.Handle(method, path, sut, func(c *gin.Context) { c.Status(http.StatusNoContent) })
 
-	// Arrange: request without keyID header, only signature.
+	// Arrange: request with invalid keyID.
 	body := []byte("some-body")
 	msgToSign := []byte(fmt.Sprintf("%s%s", body, key))
 	checksum := util.ComputeSHA256HMAC(msgToSign, key)
 	signature := base64.StdEncoding.EncodeToString(checksum)
 	req := httptest.NewRequest(method, path, bytes.NewBuffer(body))
+	req.Header.Add("BestWallet-Signature", signature)
+	req.Header.Add("BestWallet-Key-ID", "invalid-key")
+
+	// Act: handle request.
+	resp := httptest.NewRecorder()
+	router.ServeHTTP(resp, req)
+
+	// Assert.
+	if resp.Code != http.StatusUnauthorized {
+		t.Fatalf("expected response code %d, got %d", http.StatusUnauthorized, resp.Code)
+	}
+}
+
+func TestHMACVerifier_EmptyBody(t *testing.T) {
+	// Arrange: secret key and id.
+	keyID := "some-key-id"
+	key, err := util.NewKeyHMAC(64)
+	if err != nil {
+		t.Fatalf("error generating a key: %s", err)
+	}
+
+	// Arrange: the SUT (system under test).
+	sut := HMACVerifier(NewHMACKeyFetcherMock(map[string][]byte{keyID: key}))
+
+	// Arrange: mock gin router.
+	router := gin.New()
+	method, path := "GET", "/sut"
+	router.Handle(method, path, sut, func(c *gin.Context) { c.Status(http.StatusNoContent) })
+
+	// Arrange: signed request with empty body.
+	emptyBody := []byte{}
+	msgToSign := []byte(fmt.Sprintf("%s%s", emptyBody, key))
+	checksum := util.ComputeSHA256HMAC(msgToSign, key)
+	signature := base64.StdEncoding.EncodeToString(checksum)
+	req := httptest.NewRequest(method, path, bytes.NewBuffer(emptyBody))
 	req.Header.Add("BestWallet-Signature", signature)
 	req.Header.Add("BestWallet-Key-ID", "invalid-key")
 
